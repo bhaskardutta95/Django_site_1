497b0673260508bd760a38adeb3ceaf09311f658
Ashwanimehra <ashwani.mehra37@gmail.com>
2019-04-16 16:04:33 +0530

    Resolve the issue with 'Contact' link

--- theSite/secondWebapp/urls.py
+++ theSite/secondWebapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
-    url(r'^contact/', views.contact, name='contact'),
+    path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
     #path('contact/', views.contact, name='contact'),
 ]
